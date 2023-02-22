9ff0c339b67bc7ce867e8f08802e5c6dd9556e90
matthaeus <matthaeus.malek@aclue.de>
2023-02-22 13:26:43 +0100

    initial commit

---
+++ info.md
@@ -0,0 +1,4 @@
+# 
+> run the script with: go run main.go >> results.json
+#
+> this will print out the results of the script and save it to a file named "results.json"
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Open the CSV file
+	file, err := os.Open("results_with_KIF.csv")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	// Read the CSV file
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Get the header row
+	header := records[0]
+
+	// Loop through the records and print out each one as a separate JSON object
+	for _, record := range records[1:] {
+		m := make(map[string]interface{})
+		for i, value := range record {
+			// Check if value is a JSON object
+			var jsonMap map[string]interface{}
+			if json.Unmarshal([]byte(value), &jsonMap) == nil {
+				m[header[i]] = jsonMap
+			} else {
+				m[header[i]] = value
+			}
+		}
+
+		// Convert the map to a JSON object
+		jsonData, err := json.Marshal(m)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// Print the JSON object
+		fmt.Println(string(jsonData))
+	}
+}
